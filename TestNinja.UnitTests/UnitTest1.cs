bfff7ee0634980239f49d7d9a70b3e0d11afd057
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2020-02-12 00:53:59 +0530

    Initial Commit

---
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBECancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+    }
+}
